a336bb351eca24cc4e2eb03d543d69de709fa41b
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 02:53:54 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for counter in 1..number_of_times do
+  for counter in 1..7 do
     puts "#{phrase}"
   end
   # Code your solution here using the "loop" keyword to puts out the below phrase
