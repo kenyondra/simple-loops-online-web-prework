061f5c1ae0d2a976bfbc091ea5d761ae509dc803
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 02:58:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,6 +16,7 @@ def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for counter in 1..number_of_times do
     puts "#{phrase}"
+  end
 end
 
 def while_iterator(number_of_times)
